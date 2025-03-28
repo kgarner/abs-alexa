2ae712eeac2c1512373e859fca09ddb1398c85a3
kgarner <korygarner@gmail.com>
2025-03-28 12:04:23 -0600

    Update config.js

--- lambda/config.js
+++ lambda/config.js
@@ -1,9 +1,9 @@
 // config.js
 module.exports = {
-    ABS_API_KEY: 'xxxxxx',
-    SERVER_URL: 'https://abs.domain.tld',
+    ABS_API_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjNTE0NTc1MC02YzUwLTRlYTctODc2OC1mM2E5YzA0N2QyMjYiLCJ1c2VybmFtZSI6ImFsZXhhIiwiaWF0IjoxNzQzMTgyOTk0fQ.OgH1G4lmIiVafbLytjUV0gRl8cxlQ28NMdKRpUbZAFg',
+    SERVER_URL: 'https://books.garnerhome.net',
     USER_AGENT: 'AlexaSkill'
     // OTHER OPTIONAL HEADERS
-    // CFAccessClientId: 'xxxxxxxxx.access',
-    // CFAccessClientSecret: 'xxxxxxxx'
+    CFAccessClientId: '3b1465ca66805d1e03c634c076af8151.access',
+    CFAccessClientSecret: 'c034f37c311054fe29744321544f9ef611007173630789dc3ea2ea424b4f5274'
 };
